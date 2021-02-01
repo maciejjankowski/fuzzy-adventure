fd083c7757c7f63b85211cd6a7d073d6c1ea653b
maciejjankowski <2905995+maciejjankowski@users.noreply.github.com>
2021-02-01 15:47:22 +0100

    fix szukania produktów

--- main.py
+++ main.py
@@ -1,32 +1,93 @@
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
 
-from shoper_auth import login_to_session
 import json
+from shoper_api import login_to_session, create_product_api, create_category_api, GenericApiException
+from shoper_dicts import create_category_data, create_product_data
+from csv_operation import append_dict_as_row
+from local_settings import shop_url
+from shoper_category import create_category
+from shoper_product import create_product, find_product
 
 
-def main():
+false = False
+urs_err = 0
 
-    session = login_to_session(
-        shop_url=shop_url,
-        client_id=client_id,
-        client_secret=client_secret
-    )
+transport = AIOHTTPTransport(url="https://www.kramp.com/graphql/checkout-app",
+                             headers={"ctx-locale": "pl_PL"})
 
-    # print(session.headers)
-    # for i in range(len(list_of_categories)):
-    #     response = session.post(
-    #         url=shop_url + '/webapi/rest/categories',
-    #         data=json.dumps(list_of_categories[i])
-    #     )
-    #     print(response.status_code)
-    #     print(response.text, "\n")
+client = Client(transport=transport, fetch_schema_from_transport=True)
 
 
 
-try:
-    from local_settings import *
-except ImportError:
-    print("Please create local file 'local_settings.py'\n")
+
+
+
+def add_categories_to_queue():
+    cats = [{
+        "id": "web1-4045064"
+    }, {
+        "id": "web1-4045063"
+    }, {
+        "id": "web1-4045065"
+    }, {
+        "id": "web1-4045066"
+    }, {
+        "id": "web2-4045755"
+    }]
+
+    for cat_id in cats:
+        category = get_category_by_id(cat_id["id"])
+        process_category(category['category'])
+
+
+
+def process_category(cat: dict, parent=0):
+    old_id = cat['id']  # original category reference (KRAMP)
+    name = cat['name']
+    created_category = create_category(old_id, name, parent, session=session)
+    if cat.get('childCategories') and len(cat['childCategories']):
+        for child_category in cat['childCategories']:
+            process_category(child_category, created_category)  # created_category == parent_id
+
+
+
+def process_product(downloaded_product):
+    product = prepare_product_data(downloaded_product)
+    try:
+        create_or_update(product)
+    except:
+        log_error("product failed", product)
+
+
+def create_or_update_product():
+    if find_product_by_id("Kcostam1"):
+        update_product_by_id(...)
+    else:
+        # create_product(..., session=session)
+        pass
+
+
+
+
+def start_scraping():
+    add_categories_to_queue()
+    product_remaining_count = get_products_count()
+
+    while product_remaining_count > 0:
+        products_list = fetch_products()
+        add_products_to_upload_queue(products_list)
+
+    while category_remaining_count > 0:
+        category_list = fetch_categories()
+        add_categories_to_upload_queue(category_list)
+
 
 
 if __name__ == '__main__':
-    main()
+    # start_scraping()
+    session = login_to_session()
+    products = find_product(name="kluski", session=session)
+
+    print(json.dumps(products))
+    
