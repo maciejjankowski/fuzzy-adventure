0e88c5dc63d19f76bed7676ae74f82aeae50e0e9
maciejjankowski <2905995+maciejjankowski@users.noreply.github.com>
2021-04-06 14:40:12 +0200

    error handling for login

--- queue_ops.py
+++ queue_ops.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, String, Integer, and_, or_
 
 from alchemy.base import Base, session_factory
 from time import time
+import json
 
 Session = session_factory()
 
@@ -13,6 +14,7 @@ class QueueItem(Base):
     """
     __tablename__ = 'queue_item'
     record_id = Column(Integer, primary_key=True)
+    record_data = Column('data', String )
     record_type = Column('record_type', String(32))
     record_status = Column('record_status', String(32))
     last_updated = Column('last_updated', Integer)
@@ -20,7 +22,7 @@ class QueueItem(Base):
     reason = Column('reason', String(255))
 
     def __init__(self, data, record_type):
-        self.data = data
+        self.data = json.dumps(data)
         self.record_type = record_type
         self.record_status = "new"
         self.last_updated = time()
